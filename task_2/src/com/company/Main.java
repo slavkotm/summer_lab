ab18eee9ce93fbc8a26b0c8294d70977d00e8b9b
bulo4ka <kot.slava.mih@gmail.com>
2021-09-01 21:58:28 +0300

    commit

--- task_2/src/com/company/Main.java
+++ task_2/src/com/company/Main.java
@@ -1,8 +1,52 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Списки, стеки или очереди T(1..n) и U(1..n) содержат результаты n-измерений
+    тока и напряжения на неизвестном сопротивлении R.
+    Найти приближенное число R методом наименьших квадратов.
+*/
+
 public class Main {
 
+    public static List<Double> table_I() {
+        ArrayList<Double> list = new ArrayList<>();
+        double number = 0.1;
+        for(int i = 0; i < 20; i++) {
+            number += 0.1;
+            list.add(number);
+        }
+        return list;
+    }
+
+    public static List<Double> table_U() {
+        ArrayList<Double> list = new ArrayList<>();
+        double number = 0.27;
+        for(int i = 0; i < 20; i++) {
+            number += 0.27;
+            list.add(number);
+        }
+        return list;
+    }
+
+    public static double find_R(List<Double> T, List<Double> U) {
+        double R = 0.0;
+        if(T.size() == U.size())
+        {
+            double temp_I = 0.0;
+            double temp_U_I = 0.0;
+            for (int i = 0; i < T.size(); i++) {
+                temp_U_I = temp_U_I + (T.get(i) * U.get(i));
+                temp_I = temp_I + Math.pow(T.get(i), 2);
+            }
+            R = temp_U_I / temp_I;
+        }
+        return R;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+      System.out.println("Приближенное значение сопротивления: " + find_R(table_I(), table_U()));
     }
 }
